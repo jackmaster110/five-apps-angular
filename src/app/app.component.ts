363ac693d6797b25a85150c144e2f8aa444c5c7c
Samuel Shear <dspfgh34@gmail.com>
2021-08-17 01:26:20 -0400

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+type Note = {
+  title: string;
+  desc: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+  notes: Array<Note> = [];
+  title!: string;
+  body?: string;
+
+  constructor() {
+    const data = localStorage.getItem("notes");
+    if (data) this.notes = JSON.parse(data);
+  }
+
+  submitForm() {
+    let note: Note = {
+      title: this.title,
+      desc: this.body || ""
+    }
+    this.notes.push(note);
+    localStorage.setItem("notes", JSON.stringify(this.notes));
+  }
 }
